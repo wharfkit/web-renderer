f138413da343f6a19c60189605a7502939a40e34
Aaron Cox <aaron@greymass.com>
2023-01-23 17:46:58 -0500

    Declaring exports used across Svelte and Typescript

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="svelte" />
+
+declare module '*.svelte' {
+    export {SvelteComponentDev as default} from 'svelte/internal'
+    export const version: string
+    export const showModal
+    export const status
+}
