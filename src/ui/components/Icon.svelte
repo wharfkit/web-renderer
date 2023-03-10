16a7821ef2ac73eb7d9c60dffc8fb8c097f37edc
Dean Sallinen <dean@greymass.com>
2023-03-09 17:09:29 -0800

    simplify icon index

--- src/ui/components/Icon.svelte
+++ src/ui/components/Icon.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
-    import close from './icons/CloseIcon.svelte'
-    import filecode from './icons/FileCode.svelte'
-    import wharf from './icons/Wharf.svelte'
-    import login from './icons/LogIn.svelte'
-    import check from './icons/Check.svelte'
-    import ChevronRight from './icons/ChevronRight.svelte'
-    import ChevronLeft from './icons/ChevronLeft.svelte'
+    import icons from './icons/index'
 
     export let name: keyof typeof icons
-    // export let size: string = 'var(--space-l)'
-
-    const icons = {
-        close,
-        filecode,
-        wharf,
-        login,
-        check,
-        'chevron-right': ChevronRight,
-        'chevron-left': ChevronLeft,
-    }
+    //
 </script>
 
 <svelte:component this={icons[name]} />
---
+++ src/ui/components/icons/index.ts
@@ -0,0 +1,19 @@
+import Copy from './Copy.svelte'
+import Check from './Check.svelte'
+import Close from './CloseIcon.svelte'
+import FileCode from './FileCode.svelte'
+import Wharf from './Wharf.svelte'
+import Login from './LogIn.svelte'
+import ChevronRight from './ChevronRight.svelte'
+import ChevronLeft from './ChevronLeft.svelte'
+
+export default {
+    copy: Copy,
+    check: Check,
+    close: Close,
+    'file-code': FileCode,
+    wharf: Wharf,
+    login: Login,
+    'chevron-right': ChevronRight,
+    'chevron-left': ChevronLeft,
+}
