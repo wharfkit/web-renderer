8e453de08aa802e1cac0d11d2418c4cba248e96a
Dean Sallinen <dean@greymass.com>
2023-04-11 10:29:40 -0700

    remove size tokens in svgs

--- src/ui/components/icons/Expand.svelte
+++ src/ui/components/icons/Expand.svelte
@@ -1,11 +1,7 @@
-<script lang="ts">
-    export let size: string = 'var(--space-l)'
-</script>
-
 <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="100%"
-    height="100%"
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
