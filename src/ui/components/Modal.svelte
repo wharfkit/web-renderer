fc2613d06b6db68fbb914526b594ef5c51d3daa3
Dean Sallinen <dean@greymass.com>
2023-04-14 15:11:28 -0700

    add dark mode and fix typography font family

--- src/ui/components/Modal.svelte
+++ src/ui/components/Modal.svelte
@@ -64,7 +64,6 @@
 
     dialog {
         --margin-top: var(--space-xl);
-        // font-family: system-ui, ui-sans-serif;
         font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
             Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         margin-bottom: 0;
--- src/ui/components/Textarea.svelte
+++ src/ui/components/Textarea.svelte
@@ -13,16 +13,19 @@
 <style lang="scss">
     div {
         display: flex;
-        background-color: var(--swell-mist);
+        background-color: var(--text-area-background);
         padding: var(--space-m);
         border-radius: var(--border-radius-inner);
     }
     textarea {
         flex: 1;
-        color: var(--wharf-blue);
-        background-color: var(--swell-mist);
+        color: var(--text-area-text-color);
+        background-color: var(--text-area-background);
         border: none;
         font-size: var(--fs-0);
+        font-weight: 600;
+        line-height: 1.5;
         resize: none;
+        opacity: 0.75;
     }
 </style>
