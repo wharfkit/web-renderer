df25b29c5a8fbaad16f3750d686b0d718d627a38
Aaron Cox <aaron@greymass.com>
2023-04-18 11:21:41 -0400

    Containing variable size logos

--- src/ui/components/ListItem.svelte
+++ src/ui/components/ListItem.svelte
@@ -11,13 +11,15 @@
 
 <li>
     <button on:click={onClick}>
-        {#if logo}
-            <img class="logo leading" src={logo} alt={''} width="32" height="32" />
-        {:else if leadingIcon}
-            <div class="icon leading">
-                <Icon name={leadingIcon} />
-            </div>
-        {/if}
+        <div class="leading">
+            {#if logo}
+                <img class="logo" src={logo} alt={''} />
+            {:else if leadingIcon}
+                <div class="icon">
+                    <Icon name={leadingIcon} />
+                </div>
+            {/if}
+        </div>
 
         <span>{label}</span>
 
@@ -52,10 +54,19 @@
         padding-block: var(--space-s);
     }
 
-    :is(.icon, .logo).leading {
+    .leading {
         flex-basis: var(--space-xl);
         display: grid;
         place-content: center;
+        height: 32px;
+        min-height: 32px;
+        width: 32px;
+        min-width: 32px;
+        .icon,
+        .logo {
+            max-width: 32px;
+            max-height: 32px;
+        }
     }
 
     .icon.trailing {
