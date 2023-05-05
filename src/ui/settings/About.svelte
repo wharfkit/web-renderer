eccb70e7e7e2ecb7ea13fd23d17797f2fda2c7dd
Dean Sallinen <dean@greymass.com>
2023-05-05 09:26:30 -0700

    add about page

---
+++ src/ui/settings/About.svelte
@@ -0,0 +1,31 @@
+<script>
+    import BodyText from '../components/BodyText.svelte'
+    import BodyTitle from '../components/BodyTitle.svelte'
+    import Button from '../components/Button.svelte'
+</script>
+
+<div>
+    <!-- <img src="https://greymass.com/greymass.png" alt="Greymass" class="w-32 h-32 mx-auto" /> -->
+    <BodyTitle>Version 1.39.5</BodyTitle>
+    <BodyText>
+        Built by Greymass, Licenses, Thanks, Shipping great code starts with a solid platform. A Web
+        Client SDK being developed by Greymass and funded by blockchains within the Antelope
+        Coalition.
+    </BodyText>
+    <Button
+        data={{
+            label: 'View on Github',
+            href: '',
+        }}
+    />
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: var(--space-m);
+    }
+</style>
