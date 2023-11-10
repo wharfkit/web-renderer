5267894ddfcfbda33490d414a730d5924bed8e6e
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2023-11-10 09:16:30 -0800

    Account Creation Plugin Support (#132)
    * feat: add CreateAccount component
    
    * chore: added account creation example
    
    * chore: adding onAccountCreate implementation
    
    * fix: update imported plugin type
    
    * chore: added onAccountCreateComplete implementation
    
    * fix: resetting state properly after dialog
    
    * fix: giving the UI time to set the state before completing
    
    * cleanup: code cleanup
    
    * Removing and ignoring yarn-error.log
    
    * `make format`
    
    * Fixed dependencies
    
    * removed chains
    
    * Changed text to "Copy to clipboard" for non-request purposes
    
    * Few tweaks for SessionKit changes
    
    * Added account creation + custom buttons
    
    * Fixed logic with current plugin and chain selection interfaces
    
    * fix: chain selector should never be displayed when requiresChainSelect plugin config is false
    
    fix: displaying chain selector when applicable when uiRequirement.requiresChainSelect is true
    
    ---------
    
    Co-authored-by: dafuga <danielfugere28@gmail.com>
    Co-authored-by: Aaron Cox <aaron@greymass.com>

---
+++ src/ui/createAccount/AccountPlugin.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import {createEventDispatcher, setContext} from 'svelte'
+    import {AbstractAccountCreationPlugin, Checksum256} from '@wharfkit/session'
+    import List from '../components/List.svelte'
+    import ListItem from '../components/ListItem.svelte'
+    import BodyTitle from '../components/BodyTitle.svelte'
+    import {getThemedLogo} from '../../lib/utils'
+
+    export let plugins: AbstractAccountCreationPlugin[]
+    export let title: string
+
+    const dispatch = createEventDispatcher<{
+        select: string
+        cancel: void
+    }>()
+</script>
+
+{#if plugins}
+    <section>
+        <BodyTitle>{title}</BodyTitle>
+        <List>
+            {#each plugins as plugin}
+                <ListItem
+                    label={plugin.name}
+                    onClick={() => dispatch('select', plugin.id)}
+                    leadingIcon="wharf"
+                    logo={getThemedLogo(plugin.metadata)}
+                />
+            {/each}
+        </List>
+    </section>
+{/if}
+
+<style lang="scss">
+    section {
+        display: flex;
+        flex-direction: column;
+        gap: var(--space-s);
+    }
+</style>
