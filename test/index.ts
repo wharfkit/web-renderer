efb4fdd2cb210fc903325598d3070f4887677288
aaroncox <aaron.cox@greymass.com>
2026-07-28 17:07:25 -0700

    Remove Wombat wallet plugin

--- test/index.ts
+++ test/index.ts
@@ -24,7 +24,6 @@ export * from '@wharfkit/wallet-plugin-mock'
 export * from '@wharfkit/wallet-plugin-privatekey'
 export * from '@wharfkit/wallet-plugin-scatter'
 export * from '@wharfkit/wallet-plugin-tokenpocket'
-export * from '@wharfkit/wallet-plugin-wombat'
 
 // Re-export SessionKey components explicitly for test page access
 export {SessionKeyLoginPlugin, SessionKeyManager, SessionKeyWalletPlugin} from '@wharfkit/session'
